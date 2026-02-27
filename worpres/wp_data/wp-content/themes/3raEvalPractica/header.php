d60eb6c39a0917126a1593d1aaf43a056fa94e9c
kelvis23 <jesuscuestas116@gmail.com>
2026-02-27 19:33:35 +0100

    examen-page

--- worpres/wp_data/wp-content/themes/3raEvalPractica/header.php
+++ worpres/wp_data/wp-content/themes/3raEvalPractica/header.php
@@ -9,28 +9,31 @@
     <?php wp_head(); ?>
 </head>
 
-<body>
-    <header class="site-header  py-3">
+<body style="background-color:#ffc107 ;">
+ <header class="site-header py-3">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
+                
                 <div class="d-flex align-items-center gap-3">
-                    <p class="text-warning m-0 fs-4">
-                        3ra evaluación 2026
+                    <p class="text-warning m-0 fw-bold">
+                        3ra evaluacion 2026
                     </p>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpeg" alt="Logo Site" width="00">
-
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpeg" 
+                         alt="Logo Site" 
+                         class="logo-img">
                 </div>
 
                 <?php
                 $args = array(
                     'theme_location' => 'menu-principal',
-                    'container' => 'nav',
-                    'container_class' => 'menu-principal'
+                    'container'      => 'nav',
+                    'container_class'=> 'menu-principal',
+                    'fallback_cb'    => false,
                 );
                 wp_nav_menu($args);
                 ?>
+
             </div>
-        </div>
         </div>
     </header>
 </body>
--- worpres/wp_data/wp-content/themes/3raEvalPractica/page.php
+++ worpres/wp_data/wp-content/themes/3raEvalPractica/page.php
@@ -18,33 +18,39 @@
 
             </div>
         </nav>
-        <div id ="contenido" class="container">
-            <h1 class ="text-center text-warning "><?php the_title(); ?></h1>
+        <div id="contenido" class="container">
+            <h1 class="text-center text-warning "><?php the_title(); ?></h1>
             <div class="row">
+                <div class="row mb-4">
 
-                <div class="col-md-4">
-                    <?php
-                    $args = array(
-                        'theme_location' => 'menu-principal',
-                        'container' => 'nav',
-                        'container_class' => 'menu-principal'
-                    );
-                    wp_nav_menu($args);
-                    ?>
+                    <div class="col-md-4">
+                        <div class="card bg-black border-warning border-2 rounded-4 p-3">
+                            <h6 class="text-warning fw-bold mb-3">MENÚ</h6>
+                            <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'menu-principal',
+                                'container' => 'nav',
+                                'container_class' => 'menu-columna',
+                                'fallback_cb' => false,
+                            ));
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-md-8 text-white">
+
+                        <?php if (has_post_thumbnail()): ?>
+                            <div class="mb-4 text-center">
+                                <?php the_post_thumbnail('blog', array('class' => 'imagen-destacada img-fluid rounded-4')); ?>
+                            </div>
+                        <?php endif; ?>
+                        <div>
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="col-md-8">
-                    <p>
-                        Aquí va tu contenido principal. Texto, imágenes, lo que necesites.
-                    </p>
-                </div>
-
-
             </div>
         </div>
-
     </div>
-    <?php get_footer(); ?>
 </body>
 
 </html>
